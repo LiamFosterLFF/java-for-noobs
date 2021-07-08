249aac570353397a39e933b0a1da94e2b2a988ed
lf639 <lf639@bath.ac.uk>
2021-07-08 14:06:15 +0100

    First commit :rocket:

---
+++ backend/data.js
@@ -0,0 +1,14 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'coffee bean',
+            category: 'coffee',
+            brand: "Folgers", 
+            image: "",
+            price: 10,
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+}
