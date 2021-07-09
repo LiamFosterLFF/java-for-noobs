36d129dc753d504bbbbe53d7ba6ce8e37567ffbf
lf639 <lf639@bath.ac.uk>
2021-07-09 14:56:58 +0100

    Backend now connects to MongoDB to deliver products to frontend :building_construction:

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,17 @@ import express from 'express';
 import data from './data/products';
 import connectDB from './config/db';
 require('dotenv').config();
+import productRoutes from './routes/productRoutes';
 
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use('/api/products', productRoutes);
+
 const PORT = process.env.PORT || 5000
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products)
-});
 
 app.listen(PORT, () => { console.log(`Server started at http://localhost:${PORT}`) });
 
