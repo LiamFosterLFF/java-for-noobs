d244471fe694c9d91367c556c016ae8099af4a2f
lf639 <lf639@bath.ac.uk>
2021-07-23 16:10:37 +0100

    Prepped for Heroku deployment :superhero:

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import connectDB from './config/db';
+const express = require('express');
+const SMTPPool = require('nodemailer/lib/smtp-pool');
+const connectDB = require('./config/db');
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+const path = require('path');
 
-import errorHandler from './middleware/errorMiddleware';
+const errorHandler = require('./middleware/errorMiddleware');
 
 connectDB();
 
@@ -17,6 +19,18 @@ app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/private', require('./routes/privateRoutes'));
 // Error (last)
 app.use(require('./middleware/errorMiddleware'))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,'..', '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("API running");
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
