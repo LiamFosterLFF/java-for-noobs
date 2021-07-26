ed8be091c3f022e419e8f8dca5aba9f3e559dddc
lf639 <lf639@bath.ac.uk>
2021-07-27 00:28:33 +0100

    Added twitter login :bird:

--- frontend/src/components/SocialLogin.js
+++ frontend/src/components/SocialLogin.js
@@ -1,5 +1,5 @@
 import { Button, Icon } from 'semantic-ui-react';
-import { facebookProvider, githubProvider, googleProvider } from './config/firebase_auth_methods'
+import { facebookProvider, githubProvider, googleProvider, twitterProvider } from './config/firebase_auth_methods'
 import socialMediaAuth from './service/auth';
 
 const SocialLogin = () => {
@@ -13,23 +13,14 @@ const SocialLogin = () => {
             <Button color='facebook' onClick={() => handleOnClick(facebookProvider)}>
                 <Icon name='facebook' /> Facebook
             </Button>
-            <Button color='twitter'>
+            <Button color='twitter' onClick={() => handleOnClick(twitterProvider)}>
                 <Icon name='twitter' /> Twitter
             </Button>
             <Button color='google plus' onClick={() => handleOnClick(googleProvider)}>
-                <Icon name='google plus' /> Google Plus
+                <Icon name='google' /> Google
             </Button>
             <Button color='github' onClick={() => handleOnClick(githubProvider)}>
                 <Icon name='github' /> Github
-            </Button>
-            <Button color='linkedin'>
-                <Icon name='linkedin' /> LinkedIn
-            </Button>
-            <Button color='instagram'>
-                <Icon name='instagram' /> Instagram
-            </Button>
-            <Button color='youtube'>
-                <Icon name='youtube' /> YouTube
             </Button>
         </div>
     )
--- frontend/src/components/config/firebase_auth_methods.js
+++ frontend/src/components/config/firebase_auth_methods.js
@@ -3,3 +3,4 @@ import firebase from './firebase_config';
 export const facebookProvider = new firebase.auth.FacebookAuthProvider();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const githubProvider = new firebase.auth.GithubAuthProvider();
+export const twitterProvider = new firebase.auth.TwitterAuthProvider();
